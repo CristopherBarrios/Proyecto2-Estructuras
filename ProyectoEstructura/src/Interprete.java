a4eeb44d7808f1b3044b1b4a129c67338cb978e6
ricardoVA999 <40800457+ricardoVA999@users.noreply.github.com>
2019-03-14 20:18:09 -0600

    Add files via upload

--- ProyectoEstructura/src/Interprete.java
+++ ProyectoEstructura/src/Interprete.java
@@ -3,6 +3,9 @@ import java.util.List;
 import java.util.Map;
 import java.util.Stack;
 
+/**
+ * Clase que traduce los comandos ingresados por el usuario, evalua palabras claves y operandos
+ */
 public class Interprete {
     public String interpret (String[] terminos){
         Boolean inicio = false;
@@ -14,90 +17,93 @@ public class Interprete {
         Map funciones = new HashMap();
         while(seguir){
             for(int i = 0; i < terminos.length; i++){
-                if (terminos[i].equals(")")){
-                    inicio = true;
-                    cont1++;
-                }else{
-                    seguir = false;
-                }
-                if (terminos[i].equals("+") && inicio){
-                    System.out.println("Sumar");
-                    for(int x=0; x<myStack.size(); x++ ){
-                        if (myStack.size()!= 1 ){
-                            double a = myStack.pop();
-                            double b = myStack.pop();
-                            resultado = Double.toString(a+b);
-                            myStack.push(Double.parseDouble(resultado));
-                        }
+                Double numero;
+                try{
+                    numero=Double.parseDouble(terminos[i]);
+                    myStack.push(numero);
+                }catch(NumberFormatException ex){
+                    if (terminos[i].equals(")")){
+                        inicio = true;
+                        cont1++;
+                    }else{
+                        seguir = false;
                     }
-                }
-                if (terminos[i].equals("-") && inicio){
-                    System.out.println("Restar");
-                    for(int x=0; x<myStack.size(); x++ ){
-                        if (myStack.size()!= 1 ){
-                            double a = myStack.pop();
-                            double b = myStack.pop();
-                            resultado = Double.toString(a-b);
-                            myStack.push(Double.parseDouble(resultado));
-                        }
-                    }
-                }
-                if (terminos[i].equals("*") && inicio){
-                    System.out.println("Multiplicar");
-                    for(int x=0; x<myStack.size(); x++ ){
-                        if (myStack.size()!= 1 ){
-                            double a = myStack.pop();
-                            double b = myStack.pop();
-                            resultado = Double.toString(a*b);
-                            myStack.push(Double.parseDouble(resultado));
-                        }
-                    }
-                }
-                if (terminos[i].equals("/") && inicio){
-                    System.out.println("Dividir");
-                    for(int x=0; x<myStack.size(); x++ ){
-                        if (myStack.size()!= 1 ){
-                            double a = myStack.pop();
-                            double b = myStack.pop();
-                            if (b != 0){
-                                resultado = Double.toString(a/b);
+                    if (terminos[i].equals("+") && inicio){
+                        System.out.println("Sumar");
+                        for(int x=0; x<myStack.size(); x++ ){
+                            if (myStack.size()!= 1 ){
+                                double a = myStack.pop();
+                                double b = myStack.pop();
+                                resultado = Double.toString(a+b);
                                 myStack.push(Double.parseDouble(resultado));
-                            }else{
-                                System.out.println("No se puede dividir por 0");
                             }
                         }
                     }
-                }
-                if (terminos[i].equals(">") && inicio){
-                    System.out.println("Mayor");
-                    double a = myStack.pop();
-                    double b = myStack.pop();
-                    if(a>b){
-                        resultado = "Verdadero";
-                    }else{
-                        resultado = "Falso";
+                    if (terminos[i].equals("-") && inicio){
+                        System.out.println("Restar");
+                        for(int x=0; x<myStack.size(); x++ ){
+                            if (myStack.size()!= 1 ){
+                                double a = myStack.pop();
+                                double b = myStack.pop();
+                                resultado = Double.toString(a-b);
+                                myStack.push(Double.parseDouble(resultado));
+                            }
+                        }
                     }
-                }
-                if (terminos[i].equals("<") && inicio){
-                    System.out.println("Menor");
-                    double a = myStack.pop();
-                    double b = myStack.pop();
-                    if(a>b){
-                        resultado = "Falso";
-                    }else{
-                        resultado = "Verdadero";
+                    if (terminos[i].equals("*") && inicio){
+                        System.out.println("Multiplicar");
+                        for(int x=0; x<myStack.size(); x++ ){
+                            if (myStack.size()!= 1 ){
+                                double a = myStack.pop();
+                                double b = myStack.pop();
+                                resultado = Double.toString(a*b);
+                                myStack.push(Double.parseDouble(resultado));
+                            }
+                        }
                     }
-                }
-                if (terminos[i].equals("(") && inicio && cont1-1==cont2){
-                    System.out.println("Finalizado");
-                    cont2++;
-                }
-                if (terminos[i].equals("0")||terminos[i].equals("1")||terminos[i].equals("2")||terminos[i].equals("3")||terminos[i].equals("4")||terminos[i].equals("5")||terminos[i].equals("6")||terminos[i].equals("7")||terminos[i].equals("8")||terminos[i].equals("9")){
-                    myStack.push(Double.parseDouble(terminos[i]));
-                }
-                if (terminos[i].equals("Defun")){
-                    funciones.put(terminos[i], "");
-                    funciones.get(terminos[i]);
+                    if (terminos[i].equals("/") && inicio){
+                        System.out.println("Dividir");
+                        for(int x=0; x<myStack.size(); x++ ){
+                            if (myStack.size()!= 1 ){
+                                double a = myStack.pop();
+                                double b = myStack.pop();
+                                if (b != 0){
+                                    resultado = Double.toString(a/b);
+                                    myStack.push(Double.parseDouble(resultado));
+                                }else{
+                                    System.out.println("No se puede dividir por 0");
+                                }
+                            }
+                        }
+                    }
+                    if (terminos[i].equals(">") && inicio){
+                        System.out.println("Mayor");
+                        double a = myStack.pop();
+                        double b = myStack.pop();
+                        if(a>b){
+                            resultado = "Verdadero";
+                        }else{
+                            resultado = "Falso";
+                        }
+                    }
+                    if (terminos[i].equals("<") && inicio){
+                        System.out.println("Menor");
+                        double a = myStack.pop();
+                        double b = myStack.pop();
+                        if(a>b){
+                            resultado = "Falso";
+                        }else{
+                            resultado = "Verdadero";
+                        }
+                    }
+                    if (terminos[i].equals("(") && inicio && cont1==cont2+1){
+                        System.out.println("Finalizado");
+                        cont2++;
+                    }
+                    if (terminos[i].equals("Defun")){
+                        funciones.put(terminos[i], "");
+                        funciones.get(terminos[i]);
+                    }
                 }
             }
         }
