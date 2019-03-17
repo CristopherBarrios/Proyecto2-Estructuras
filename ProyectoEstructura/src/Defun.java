50e3e819ef5075a482df6102ad668c55a40e0ef6
Estuardo Ureta <32362738+estuardo8u14@users.noreply.github.com>
2019-03-17 09:01:36 -0600

    Update Defun.java

--- ProyectoEstructura/src/Defun.java
+++ ProyectoEstructura/src/Defun.java
@@ -1,69 +1,37 @@
-// Evaluador de expresion prefix
 
 import java.io.*;
 import java.util.*;
 
 class Defun {
 
-    static Boolean esOperando(char c)
-    {
-        // si el caracter es un digito entonces
-        //debe ser un operando
-         
-        if(c >= 48 && c <= 57)
-            return true;
-        else
-            return false;
-    }
+    protected String nombre;
+    protected Vector <String> formales;
+    protected TreeNode cuerpo;
 
-    static double evaluarPrefijo(String exprsn)
-    {
-        Stack<Double> Stack = new Stack<Double>();
-
-        for (int j = exprsn.length() - 1; j >= 0; j--) {
-
-            // Push del operando al Stack
-            // para convertir exprsn[j] a un digito restar
-            // '0' de exprsn[j].
-            if (esOperando(exprsn.charAt(j)))
-                Stack.push((double)(exprsn.charAt(j) - 48));
-
-            else {
-
-                // operador encontrado
-                // Pop dos elementrosd Stack
-                double a1 = Stack.peek();
-                Stack.pop();
-                double a2 = Stack.peek();
-                Stack.pop();
-
-                // uso de switch para operar en a1 y a2 y hacer a1 o a2
-
-                switch (exprsn.charAt(j)) {
-                    case '+':
-                        Stack.push(a1 + a2);
-                        break;
-                    case '-':
-                        Stack.push(a1 - a2);
-                        break;
-                    case '*':
-                        Stack.push(a1 * a2);
-                        break;
-                    case '/':
-                        Stack.push(a1 / a2);
-                        break;
-                }
-            }
+    /**
+     * función: Defun: usuario defina funciones
+     *
+     * Constructor: Defun(String n, TreeNode f, TreeNode b)
+     *
+     * esta funcion crea una función creada por el usuario con nombre, lista de paramtros formales y cuerpo
+     *
+     * @todo: deja que cuerpo no sea lista
+     *
+     * @param n el nombre de la  funcion
+     * @param f la list de formales - puede ser()
+     * @param b el cuerpo de la funcion
+     *
+     * @throws Excepcion si parametros no estan correctos
+     *
+     */
+    public Defun(String n, TreeNode f, TreeNode b) throws excepcion{
+        nombre = n;
+        if ( (!f.isList() && !f.toString().matches("NIL") ) || ( !b.isList() && !b.toString().matches("NIL") ) ){
+            throw new excepcion("funcion de parametros invalidos o cuerpo.\n" + f.toString() + "\n" + b.toString());
         }
 
-        return Stack.peek();
+        String formalesstring = f.toString();
+        formales = splitParamList(formalesstring);
+        cuerpo = b;
     }
-
-    /* programa para probar funcionamiento */
-    public static void main(String[] args)
-    {
-        String exprsn = "+9/26";
-        System.out.println(evaluarPrefijo(exprsn));
-    }
-}
 
