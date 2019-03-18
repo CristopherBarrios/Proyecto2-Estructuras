217bd975e20c5596dcbdffe0d11eed96a56a153f
Estuardo Ureta <32362738+estuardo8u14@users.noreply.github.com>
2019-03-18 16:10:40 -0600

    Update JUnits.java

--- ProyectoEstructura/src/JUnits.java
+++ ProyectoEstructura/src/JUnits.java
@@ -1,1 +1,15 @@
 
+//parser prueba
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class ParserTest {
+// prueba si lo guardado en la lista es lo esperado con assertequals
+	@Test
+	public void testParse() {
+		Parser tester = new Parser(); 
+		String operacion = ("( + 2 1)");
+		assertEquals("( + 2 1 )", operacion, tester);
+	}
+
+}
