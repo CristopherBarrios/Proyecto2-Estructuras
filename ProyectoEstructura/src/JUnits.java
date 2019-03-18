1d6f9db4cc0f53e3340bb43fea8e782d579fcc1f
Estuardo Ureta <32362738+estuardo8u14@users.noreply.github.com>
2019-03-18 16:11:46 -0600

    Update JUnits.java

--- ProyectoEstructura/src/JUnits.java
+++ ProyectoEstructura/src/JUnits.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class ParserTest {
-// prueba si lo guardado en la lista es lo esperado con assertequals
+
 	@Test
 	public void testParse() {
 		Parser tester = new Parser(); 
